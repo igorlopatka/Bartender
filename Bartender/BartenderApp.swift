0e3402b2e51835b21cb67e08a145be1c3007a419
Igor Łopatka <igor.lopatka11@gmail.com>
2022-06-10 10:06:54 +0200

    Initial Commit

---
+++ Bartender/BartenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BartenderApp.swift
+//  Bartender
+//
+//  Created by Igor Łopatka on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BartenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
