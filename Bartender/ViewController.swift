677d0b1ffce4f5ee639815d449031522b1e975cf
Igor Łopatka <igor.lopatka11@gmail.com>
2022-11-08 16:41:35 +0100

    Initial Commit

---
+++ Bartender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bartender
+//
+//  Created by Igor Łopatka on 08/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
