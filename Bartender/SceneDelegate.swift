9eafe310d109572179f9e923cad97c300b74da2a
Igor Łopatka <igor.lopatka11@gmail.com>
2022-11-16 19:22:58 +0100

    Add search bar to navigationview

--- Bartender/SceneDelegate.swift
+++ Bartender/SceneDelegate.swift
@@ -8,23 +8,23 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-            let mainVC = initViewController()
-            let navigationController = UINavigationController(rootViewController: mainVC)
-            navigationBarConfiguration(navigationController)
+        
+        let mainVC = initViewController()
+        let navigationController = UINavigationController(rootViewController: mainVC)
+        navigationBarConfiguration(navigationController)
         
         
-            window.rootViewController = navigationController
-            window.makeKeyAndVisible()
-            self.window = window
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
         guard let _ = (scene as? UIWindowScene) else { return }
     }
     
@@ -33,7 +33,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         returnTo.title = "Bartender"
         return returnTo
     }
-
+    
     private func navigationBarConfiguration (_ controller: UINavigationController) {
         controller.navigationBar.prefersLargeTitles = true
         controller.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
@@ -42,15 +42,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         controller.navigationBar.backgroundColor = .systemBackground
         
         if #available(iOS 13.0, *) {
-                    let navBarAppearance = UINavigationBarAppearance()
-                    navBarAppearance.configureWithOpaqueBackground()
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            
             navBarAppearance.backgroundColor = .systemBackground
-                    controller.navigationBar.standardAppearance = navBarAppearance
-                    controller.navigationBar.scrollEdgeAppearance = navBarAppearance
-                } else {
-                    controller.edgesForExtendedLayout = []
-                }
-
+            controller.navigationBar.prefersLargeTitles = true
+            controller.navigationBar.standardAppearance = navBarAppearance
+            controller.navigationBar.scrollEdgeAppearance = navBarAppearance
+        } else {
+            controller.edgesForExtendedLayout = []
+        }
     }
 }
 
--- Bartender/ViewController.swift
+++ Bartender/ViewController.swift
@@ -7,14 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
+    
+    //Placeholder data
+    let myArray = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+            "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+            "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+            "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+            "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
 
+    var filteredData: [String]!
+    
+    private var tableView: UITableView!
+    private var search: UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
         
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        tableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.view.addSubview(tableView)
+        
+        search = UISearchController(searchResultsController: nil)
+        search.searchResultsUpdater = self
+        search.searchBar.backgroundColor = .red
+        search.searchBar.barStyle = .default
+        search.searchBar.placeholder = "What do you have in your bar?"
+        search.obscuresBackgroundDuringPresentation = true
+        
+        navigationItem.searchController = search
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(myArray[indexPath.row])")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(myArray[indexPath.row])"
+        return cell
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        print(text)
     }
 }
 
