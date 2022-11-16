7c774a2313047679fea76d71af6163f6a4b6fec7
Igor Łopatka <igor.lopatka11@gmail.com>
2022-11-16 11:57:56 +0100

    Set up navigation bar

--- Bartender/SceneDelegate.swift
+++ Bartender/SceneDelegate.swift
@@ -17,40 +17,40 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = ViewController() // Your initial view controller.
+            let mainVC = initViewController()
+            let navigationController = UINavigationController(rootViewController: mainVC)
+            navigationBarConfiguration(navigationController)
+        
+        
+            window.rootViewController = navigationController
             window.makeKeyAndVisible()
             self.window = window
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    
+    private func initViewController () -> UIViewController {
+        let returnTo = ViewController()
+        returnTo.title = "Bartender"
+        return returnTo
     }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    private func navigationBarConfiguration (_ controller: UINavigationController) {
+        controller.navigationBar.prefersLargeTitles = true
+        controller.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        controller.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        controller.navigationBar.tintColor = .white
+        controller.navigationBar.backgroundColor = .systemBackground
+        
+        if #available(iOS 13.0, *) {
+                    let navBarAppearance = UINavigationBarAppearance()
+                    navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.backgroundColor = .systemBackground
+                    controller.navigationBar.standardAppearance = navBarAppearance
+                    controller.navigationBar.scrollEdgeAppearance = navBarAppearance
+                } else {
+                    controller.edgesForExtendedLayout = []
+                }
+
     }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
