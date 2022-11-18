049f98034f078a1f586124ebf81e12c726613599
Igor Łopatka <igor.lopatka11@gmail.com>
2022-11-18 09:52:12 +0100

    Create drink details vc

---
+++ Bartender/View/DrinkDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DrinkDetailsViewController.swift
+//  Bartender
+//
+//  Created by Igor Łopatka on 18/11/2022.
+//
+
+import UIKit
+
+class DrinkDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
